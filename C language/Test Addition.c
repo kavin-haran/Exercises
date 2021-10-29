eecab4ef34dc4994cee924eea4921dc341704b73
Jo <skavinharan@gmail.com>
2021-10-29 16:31:52 +0530

    Exercises and Examples

---
+++ C language/Test Addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter two numbers to do addition");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf(" \n The addition of the two numberrs is %d",c);
+}
---
+++ Javscript/Helloworld.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+let usrname="Joseph"
+console.log(usrname)
+let user_name="Jo" //Using underscore is snake casing
+console.log(user_name)
+let userName="JoJO" //Using underscore is Camel casing
+console.log(userName)
