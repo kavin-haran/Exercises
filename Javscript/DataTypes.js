2726cfbf43e9b4112ea34dc9a139f0c726e6e92c
Jo <skavinharan@gmail.com>
2021-10-29 19:42:12 +0530

    updates for datatypes in js

---
+++ Javscript/DataTypes.js
@@ -0,0 +1,13 @@
+let num=28
+let userName="Joseph"
+console.log(typeof userName)
+console.log(typeof num) //typeof keyword to determine the type of the variable
+let num2=10_000_000_0000 // to differntiate number use _
+console.log(typeof num2)
+console.log(Number.MAX_VALUE)//largest number allowed in js
+console.log(Number.MAX_VALUE * 100) //infinity allowed in java script
+let num3=1242425142342134252 //long numbers cannot be displayed in js
+console.log(num3)
+let num4=123142142424512341234n //add n @ the end for using big int
+console.log(num4)
+console.log(num4 + 10) // cannot add a big int with a int
